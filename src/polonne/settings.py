3da7441ab24b46cf88add9a66fc66ba3ea847b92
Vasyl Dizhak <vasyl.dizhak@djangostars.com>
2011-02-05 12:24:30 +0200

    Initial commit

---
+++ src/polonne/settings.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import os.path
+import sys
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
+sys.path.append(os.path.join(PROJECT_PATH, 'apps'),)
+sys.path.append(os.path.join(PROJECT_PATH, 'plugins'),)
+sys.path.append(os.path.join(PROJECT_PATH, 'contrib'),)
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Vasyl Dizhak', 'dijakroot@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',                     
+        'USER': '',                      
+        'PASSWORD': '',                 
+        'HOST': '',                      
+        'PORT': '',                     
+    }
+}
+
+TIME_ZONE = 'Europe/Kiev'
+LANGUAGE_CODE = 'ua-ua'
+
+LANGUAGES = (
+    ('ua', ('Ukrainian')),
+    ('ru', ('Russian')),
+    ('en', ('English')),
+)
+
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+
+MEDIA_URL = '/media/'
+
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+SECRET_KEY = '=g+@l)yw3)23#(o+u=e@$c+_t!_^sk2z=@a$cpbaiav)iy#-u-'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'polonne.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'south',
+    'transmeta',
+    'sorl.thumbnail',
+    'localeurl',
+)
+
+try:
+    from local_settings import *
+except ImportError:
+    print "No local settings found"
